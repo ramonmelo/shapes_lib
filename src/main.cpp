e468265d8f0e8b8682f6e53f7313eb721c285dff
Ramon Melo <ramonmelo@ramonmelo.com>
2023-01-06 17:05:14 -0300

    Added Main Test file

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <shapes/Point.h>
+
+using namespace std;
+
+void displayPoint(Point& p) {
+  cout << p.getX() << "," << p.getY() << "," << p.getZ() << endl;
+}
+
+int main()
+{
+  Point p(1, 2, 3);
+
+  displayPoint(p);
+
+  return 0;
+}
